fba018c789c2492246ee2662c85ea4129f2c0df1
Dan Cohen <dcohen@usccreditunion.org>
2016-07-29 11:42:32 -0700

    Write script to create job yml files

--- deployable_sql/folders.py
+++ deployable_sql/folders.py
@@ -3,9 +3,14 @@ Folders to set up in the install location.
 """
 import os
 from subprocess import call
+from datetime import datetime
+
+import yaml
 
 
-FOLDERS = ['views', 'tables', 'functions', 'stored_procedures', 'permissions']
+FOLDERS = [
+    'views', 'tables', 'functions', 'stored_procedures', 'permissions', 'jobs'
+    ]
 
 FILES = [os.path.join('permissions', 'grant_deployable.sql')]
 
@@ -34,3 +39,33 @@ def run_setup(username, db, gitinit=True):
         call('git init', shell=True)
 
     print('All set!')
+
+def create_job(jobname):
+    """
+    Creates a basic job from template.
+    """
+    yml = {
+        jobname:
+            {
+                'steps': [
+                    {
+                        'step_name': jobname,
+                        'command': 'EXEC %s;' % jobname
+                    }
+                ],
+                'schedules': [
+                    {
+                        'frequency_type': 'weekly',
+                        'frequency_interval': 'sunday',
+                        'active_start_time': '060000',
+                        'active_start_date': datetime.now().strftime('%Y-%m-%d'),
+                        'schedule_name': 'weekly'
+                    }
+                ]
+            }
+        }
+    if not os.path.exists('jobs'):
+        os.mkdir('jobs')
+    with open(os.path.join('jobs', jobname + '.yml'), 'w') as stream:
+        stream.write(yaml.dump(yml, default_flow_style=False))
+    return yml
