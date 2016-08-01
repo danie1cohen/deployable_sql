f3929ff9a9e26bca49089c7801e27577a56fb11a
Dan Cohen <dcohen@usccreditunion.org>
2016-08-01 09:23:19 -0700

    Add test for multi schedules

--- tests/deployable_sql_tests.py
+++ tests/deployable_sql_tests.py
@@ -78,3 +78,14 @@ class TestDeployableSql(object):
         job_name, sql = read_job(self.job)
         eq_(job_name, 'testjob')
         assert 'EXEC sp_add_job' in sql
+
+    def test_read_job_multisched(self):
+        second_sched = {
+            'active_start_date': '2016-08-01',
+            'active_start_time': '091000',
+            'freq_type': 'once',
+            'name': 'once',
+            }
+        self.job['testjob']['schedules'].append(second_sched)
+        job_name, sql = read_job(self.job)
+        eq_(sql.count('sp_add_jobschedule'), 2)
