972c48e2367de7c05558be1891c1faf6620a999c
Dan Cohen <dcohen@usccreditunion.org>
2017-06-21 09:11:07 -0700

    Update fab to new style

--- fabfile.py
+++ fabfile.py
@@ -7,35 +7,32 @@ from __future__ import print_function
 import os
 from datetime import datetime
 
-from fabric.api import run, env, cd, prefix, put, sudo, local
+from fabric.api import *
 
 
 env.host_string = 'administrator@gelsons'
 env.user = 'administrator'
 env.key_filename = 'U:\\.ssh\\id_rsa'
+env.install_path = '/opt/deployable_sql'
+env.current = '/opt/deployable_sql/current'
+env.repo = 'git@git.usccreditunion.org:cu/deployable_sql.git'
+env.build = datetime.now().strftime('%Y%m%d%H%M%S')
+env.settings = 'settings.p'
 
-data = {
-    'install_path': '/opt/deployable_sql',
-    'current': '/opt/deployable_sql/current',
-    'repo': 'git@git.usccreditunion.org:cu/deployable_sql.git',
-    'build': datetime.now().strftime('%Y%m%d%H%M%S'),
-    'settings': 'settings.p',
-    'user': env.user
-}
 
 
 def deploy():
     """Deploys new code and handles the commands that must follow"""
-    with cd('%(install_path)s' % data):
-        run('git clone %(repo)s %(build)s' % data)
-        #run('cp current/%(settings)s %(build)s/%(settings)s' % data)
-        with cd('%(build)s' % data), prefix('workon sql'):
+    with cd('%(install_path)s' % env):
+        run('git clone %(repo)s %(build)s' % env)
+        #run('cp current/%(settings)s %(build)s/%(settings)s' % env)
+        with cd('%(build)s' % env), prefix('workon sql'):
             run('pip install -U pip')
             run('pip install -r requirements.txt')
             run('nosetests -v')
             # if tests pass, change symlink
-            remove = 'rm %(current)s' % data
-            replace = 'ln -sfn %(install_path)s/%(build)s %(current)s' % data
+            remove = 'rm %(current)s' % env
+            replace = 'ln -sfn %(install_path)s/%(build)s %(current)s' % env
             run(remove + ' && ' + replace)
             run('pip uninstall -y deployable_sql')
             run('pip install .')
@@ -46,22 +43,22 @@ def create():
     Does initial folder setup.
     """
     #local('ssh-copy-id -i %s %s' % (env.key_filename, env.host_string))
-    #put('%(settings)s' % data)
-    #sudo('chmod 700 %(settings)s' % data)
-    sudo('mkdir %(install_path)s' % data)
-    sudo('chown -R %(user)s:%(user)s %(install_path)s' % data)
-    with cd('%(install_path)s' % data):
+    #put('%(settings)s' % env)
+    #sudo('chmod 700 %(settings)s' % env)
+    sudo('mkdir %(install_path)s' % env)
+    sudo('chown -R %(user)s:%(user)s %(install_path)s' % env)
+    with cd('%(install_path)s' % env):
         run('mkdir current')
         run('mkvirtualenv sql')
-        #run('mv ~/%(settings)s %(current)s/%(settings)s' % data)
+        #run('mv ~/%(settings)s %(current)s/%(settings)s' % env)
     # set up log dir
     sudo('mkdir /var/log/deployable_sql')
-    sudo('chown -R %(user)s:%(user)s /var/log/deployable_sql' % data)
+    sudo('chown -R %(user)s:%(user)s /var/log/deployable_sql' % env)
     deploy()
 
 def oneoff():
     """Run deployable_sql as a one off process."""
-    with cd('%(current)s' % data), prefix('workon sql'):
+    with cd('%(current)s' % env), prefix('workon sql'):
         run('python bin/deploy.py auto --all')
 
 def increment_version():
@@ -91,7 +88,7 @@ def deployed_version():
     """
     Return the version of the current deployed code.
     """
-    run('cat %(current)s/setup.py | grep -i version' % data)
+    run('cat %(current)s/setup.py | grep -i version' % env)
 
 def uname():
     """
@@ -101,5 +98,5 @@ def uname():
 
 def chmod_settings():
     """Restrict file access for settings file."""
-    with cd(data['current']):
-        sudo('chmod 700 %(settings)s' % data)
+    with cd(env['current']):
+        sudo('chmod 700 %(settings)s' % env)
